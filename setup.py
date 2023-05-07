cd57014c4521b129578979bd269508d5f208461e
tk <462304@qq.com>
2023-05-07 22:24:21 +0800

    remove np.float
    Signed-off-by: tk <462304@qq.com>

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 
     setuptools.setup(
         name=package_name,
-        version="0.9.7",
+        version="0.9.7@post0",
         author="ssbuild",
         author_email="9727464@qq.com",
         description=title,
