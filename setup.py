4f504af7cfa673f475acb8b7c41697ad0b241ea8
tk <462304@qq.com>
2025-05-03 22:40:50 +0800

    0.10.0
    Signed-off-by: tk <462304@qq.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md'),
 if __name__ == '__main__':
     setup(
         name="fastdatasets",
-        version="0.9.17",
+        version="0.10.0",
         author="ssbuild",
         author_email="9727464@qq.com",
         description='fastdatasets: datasets for tfrecords',
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         include_package_data=True,
         package_data={"": ["**/*.cu", "**/*.cpp", "**/*.cuh", "**/*.h", "**/*.pyx"]},
         python_requires='>=3, <4',  # python的依赖关系
-        install_requires=['tfrecords >= 0.2.16 , < 0.3','data_serialize >= 0.2.1','numpy'],
+        install_requires=['tfrecords >= 0.3.0 , < 0.4','data_serialize >= 0.2.1','numpy'],
         classifiers=[
             'Development Status :: 5 - Production/Stable',
             'Intended Audience :: Developers',
@@ -29,13 +29,10 @@ if __name__ == '__main__':
             'License :: OSI Approved :: Apache Software License',
             'Programming Language :: C++',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
             'Programming Language :: Python :: 3.11',
             'Programming Language :: Python :: 3.12',
+            'Programming Language :: Python :: 3.13',
             'Topic :: Scientific/Engineering',
             'Topic :: Scientific/Engineering :: Mathematics',
             'Topic :: Scientific/Engineering :: Artificial Intelligence',
